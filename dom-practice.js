aacde4593d69e45a4bd346ab1170f5afc24cb2b5
Edoardo Ruiz <44981900+edoardrui0@users.noreply.github.com>
2024-04-11 16:07:36 -0400

    eventListeners

--- dom-practice.js
+++ dom-practice.js
@@ -47,3 +47,28 @@ pinkDivP.textContent = `ME TOO!`;
 
 pinkDiv.appendChild(pinkDivH1);
 pinkDiv.appendChild(pinkDivP);
+
+const btn = document.querySelector("#btn");
+
+function alertFunction() {
+  alert("YAY! YOU DID IT!");
+}
+
+btn.addEventListener("click", function (e) {
+  e.target.style.background = "blue";
+});
+
+console.log(container);
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
+});
+
+// console.log(buttons);
